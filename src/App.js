5845cf94acea709c28b88fe2ab8b63d9f793a928
navinrc <navinrc98@gmail.com>
2022-08-29 13:15:44 +0530

    FeedbackForm component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
@@ -15,6 +17,7 @@ function App() {
   return (
     <> 
       <Header />
+      <FeedbackForm />
       <div className="container">
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
