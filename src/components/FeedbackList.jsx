4a4bea6fc878f45166c9457ef717a7ed75e3e470
navinrc <navinrc98@gmail.com>
2022-08-30 22:30:01 +0530

    framermotion and router

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,27 +1,42 @@
-import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+import { AnimatePresence, motion } from "framer-motion";
 
-function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length ===0 ){
-        return <p>No feedback yet</p>
-    }
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No feedback yet</p>;
+  }
+
   return (
     <div className="feedback-list">
+      <AnimatePresence>
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
         ))}
+      </AnimatePresence>
     </div>
-  )
+  );
 }
 
 FeedbackList.propTypes = {
-  feedback : PropTypes.arrayOf(
+  feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id : PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-      text : PropTypes.string.isRequired,
-      rating : PropTypes.number.isRequired,
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
     })
-  )
-}
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
